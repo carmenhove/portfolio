099c1559aca230002d3e5e6b7bea82755719bfa4
carmenhove <hove.cm@gmail.com>
2022-04-07 14:45:49 -0700

    A whole lotta edits

--- DampChristmas.R
+++ DampChristmas.R
@@ -1,28 +1,42 @@
+
+#PROJECT B
+
+#Upload data as a csv file
+df1 <- read.csv("FullData.csv", stringsAsFactors = FALSE)
+
+#Whitespace is due to utf8 characters - and this is true across all variables
+utf8::utf8_print(unique(df1$Ave.Temp), utf8 = FALSE)
+
+#Create a function to replace all utf8 values
+replace.values <- function(x) x <- iconv(x, "latin1", "ASCII", sub="")
+
 #NOAA online data source, Seattle Metro area 2000-2021
+df2 <- df1 %>% 
+  #Remove "T" values for precipitation, no code to indicate what this value means
+  filter(!grepl("T",Precipitation, fixed = T)) %>% 
+  #Replace all utf8 values
+  mutate_all(replace.values) %>% 
+  #Fix dates, which were coded differently for 2021 versus all previous years
+  mutate(Date = case_when(grepl("/",Date,fixed = T) ~ as.Date(Date, format= "%m/%d/%y"),
+                          grepl("-",Date,fixed = T) ~ as.Date(Date, format= "%Y-%m-%d"))) %>% 
+  #Separate out Date into month,day,year
+  mutate_at(vars(Date), tibble::lst(year, month, day)) %>% 
+  #Make sure variables are in numeric format
+  mutate(across(c(Max.Temp:Precipitation,day,year), as.numeric)) %>% 
+  #Define "Freezing" and "PrecipB" binary variables for analysis
+  mutate(Freezing = case_when(Min.Temp > 33 ~ 0,
+                              Min.Temp <= 33 ~ 1),
+         PrecipB = case_when(Precipitation > 0 ~ 1,
+                             Precipitation ==0 ~ 0)) 
 
-NOAAdf1 <- read.csv("FullData.csv", stringsAsFactors = FALSE) 
-colnames(NOAAdf1)
-str(NOAAdf1)
-#table(NOAAdf1$X.Daily.Data) 
+hist(df2$Max.Temp)
 
-#Where do the mislabeled data come from?
-NOAAdf1 %>% filter(X.Daily.Data == "Temperature")
-NOAAdf1 %>% filter(Year %in% c("2011","2016"))
-#2011 and 2016 = Temperature needs to be relabeled as "Max Temperature"
+#Test model
+log.model <- glm(PrecipB ~ day + year + Ave.Temp, 
+                 data = Bdf1, family = 'binomial')
+summary(log.model)
+plot(log.model)
+summary(log.model)$coefficients
 
-NOAAdf2 <- NOAAdf1 %>% 
-  mutate(President = case_when(Year %in% c(2000:2007) ~ "Bush Jr",
-                               Year %in% c(2008:2015) ~ "Obama",
-                               Year %in% c(2016:2019) ~ "Trump",
-                               Year %in% c(2020:2021) ~ "Biden"),
-         Parameter = case_when(X.Daily.Data == "Temperature" ~ 
-                                 "Max Temperature",
-                               TRUE ~ X.Daily.Data)) %>% 
-  mutate(Parameter = gsub(" ", "_", Parameter)) %>% 
-  select(Parameter,Observed,Year,President) %>% 
-  pivot_wider(names_from = Parameter, values_from = Observed) %>% 
-  mutate(across(c(3:6), as.numeric),
-         PrecipB = case_when(Precipitation == 0 ~ "No",
-                             TRUE ~ "Yes"))
-         
-read.delim(file, header = TRUE, sep = "\t", dec = ".", ...)
+predict(log.model, 
+        newdata = list(Precipitation = c(0, 0.08,0.36)))
--- Hormones.R
+++ Hormones.R
@@ -1,8 +1,11 @@
 
-#PROJECT 1: What are the sex-specific effects of age on hormone production?
+#PROJECT A
 
 #Identify packages 
-pkgs = c("nhanesA","tidyverse","magrittr","knitr","patchwork","ggpubr","scales","brms","mgcv","schoenberg","tidymv")
+pkgs = c("nhanesA","tidyverse","magrittr","knitr",
+         "patchwork","ggpubr","scales","brms","mgcv",
+        "sjlabelled","schoenberg","tidymv","broom",
+        "broom.mixed","tidybayes","calecopal")
 
 #Install packages
 install.packages(pkgs)
@@ -14,122 +17,202 @@ inst = lapply(pkgs, library, character.only = TRUE)
 devtools::install_github("karthik/wesanderson")
 library(wesanderson)
 
-#Load data directly from NHANES
+#Rstan has been acting squirrely on my OS when I tweak adapt_delta and max treedepth. To get around this, I use cmdstranr on the back-end. 
+install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+library(cmdstanr)
+check_cmdstan_toolchain()
+install_cmdstan(cores = 2,overwrite = T)
+cmdstan_path()
+cmdstan_version()
+options(brms.backend = "cmdstanr")
+
+
+#Load data from NHANES
 ##Identify BMI, demographic, reproductive health, and hormone data files
-table.names <- sort(c('DEMO', 'RHQ','BMX', 
-                      str_c(c('DEMO','RHQ','BMX'),"_",rep(c('H','I'),each=3)), 
+table.names <- sort(c(str_c(c('DEMO','RHQ','BMX'),"_",rep(c('H','I'),each=3)), 
                       'TST_H','TST_I'))
 
 #apply nhanes() to extract SAS data frames, transition to R list
-nhanes.l1 <- sapply(table.names, nhanes) 
+nhanes.l1 <- map(table.names, nhanes) 
 
 #remove SAS labels
-nhanes.l2 <- sapply(nhanes.l1, remove_all_labels) 
+nhanes.l2 <- map(nhanes.l1, remove_all_labels)
 
-#reduce and merge data files by file type
+#Name list elements
+names(nhanes.l2)<-table.names
+
+#Reduce and merge data files by file type
 BMI <- Reduce(full_join,nhanes.l2[grepl("BMX", names(nhanes.l2))])
 RHQ <- Reduce(full_join,nhanes.l2[grepl("RHQ", names(nhanes.l2))])
 DEMO <- Reduce(full_join,nhanes.l2[grepl("DEMO", names(nhanes.l2))])
 HORM <- Reduce(full_join,nhanes.l2[grepl("TST", names(nhanes.l2))])
 
 #Merge, clean dataset
-df1 <- full_join(BMI, DEMO) %>% 
+PAdf1 <- full_join(BMI, DEMO) %>% 
   full_join(., RHQ) %>% full_join(., HORM) %>% 
   rename(Sex = RIAGENDR, Age = RIDAGEYR, BMI = BMXBMI, 
          Preg.demo = RIDEXPRG, Preg.rhq = RHD143, #Currently pregnant?
          Estradiol = LBXEST, Testosterone = LBXTST,
-         Est.cc = LBDESTLC, Test.cc = LBDTSTLC,SHBG = LBXSHBG) %>% 
+         Est.cc = LBDESTLC, Test.cc = LBDTSTLC,
+         SHBG = LBXSHBG, Hysterectomy = RHD280) %>% 
   mutate(across(c(Estradiol, Testosterone),abs), #Make sure E and T are absolute values
          Pregnant = case_when(Preg.demo == 1 | Preg.rhq == 1 ~ "Yes",TRUE ~ "No"),
          Sex = as.factor(case_when(Sex == 1 ~ "Male", Sex == 2 ~ "Female")),
          Pregnant.Sex = case_when(Sex == "Male" ~ "Male",
                                   Sex == "Female" & Pregnant == "No" ~ "NP female",
                                   Sex == "Female" & Pregnant == "Yes" ~ "P female")) %>% 
-  filter(across(c(Testosterone, Estradiol,Age,Sex), ~ !is.na(.)) &
-           (RHD280 == 2 | (Sex == "Female" & Age < 20) | Sex == "Male") & #No hysterectomy (RHD280 ==2), Not measured in any females under the age of 20
+  filter(across(c(Testosterone, Estradiol,Age,Sex), ~ !is.na(.)) & #Remove NAs
+           #No hysterectomy (not measured in any females under the age of 20)
+           (Hysterectomy == 2 | (Sex == "Female" & Age < 20) | Sex == "Male")) %>%  
            #Remove imputed values due to below-threshold detection limit reported
-           Est.cc == 0 & Test.cc == 0) %>% 
-  select(Testosterone,Estradiol,SHBG,Pregnant,Sex,Age,BMI,Pregnant.Sex)
-
-df2 <- df1 %>% 
-  pivot_longer(Testosterone:SHBG, 
-               names_to = "Measure",
-               values_to = "Value")
+           #Est.cc == 0 & Test.cc == 0) %>% 
+  select(Testosterone,Test.cc,Estradiol,Est.cc,Pregnant,Pregnant.Sex,Age,Sex,BMI) %>% 
+  mutate(Sex = ordered(Sex, levels = c("Male","Female")),
+         Pregnant.Sex = ordered(Pregnant.Sex, levels = c("Male","NP Female","P Female")))
 
 #Structure of dataset
-str(df1)
+str(PAdf1)
+
+#Pivot longer to create "Measure" variable
+PAdf2 <- PAdf1 %>% 
+  pivot_longer(c(Testosterone,Estradiol), 
+               names_to = "Measure",
+               values_to = "Value") %>% 
+  filter(!is.na(Value))
+
+str(PAdf2)
+
+#Histogram of outcome variables
+ggplot(PAdf2, aes(x = Value, color = Sex))+
+  geom_histogram(bins = 30, alpha = 0.8)+
+  facet_grid(~ Measure, scales = "free")
+
+#No zero values
+sum(PAdf2$Value==0,na.rm = T)
+
+#Log-transformation
+ggplot(PAdf2, aes(x = log(Value), color = Sex))+
+  geom_histogram(bins = 30, alpha = 0.8)+
+  facet_grid(~ Measure, scales = "free")
+
+#Remove imputed values
+PAdf3 <- PAdf2 %>%  
+  filter(Est.cc == 0 & Test.cc == 0)
 
 #Check distributions
-ggplot(df2, aes(x = log(Value), color = Sex, fill = Sex))+
+ggplot(PAdf3, aes(x = log(Value), color = Sex))+
   geom_histogram(bins = 30, alpha = 0.8)+
-  facet_grid(~ Measure,scales = "free")
+  facet_grid( ~ Measure,scales = "free")
 
-ggplot(df2, aes(x = Age, y= log(Value),color = Pregnant.Sex))+
-  geom_point(position = "jitter") + 
-  facet_grid(Measure ~., scales = "free")
+#What about pregnancy?
+table(PAdf3$Pregnant) #112 pregnant individuals in this dataset
 
-ggplot(df1, aes(x = log(Estradiol), 
-                y= log(Testosterone),color = Pregnant.Sex))+
-  geom_point(position = "jitter") #+ 
-  #facet_grid(Measure ~., scales = "free")
+#Check plots to see how pregnancy relates to Estradiol
+ggplot(PAdf3, aes(x = Age, y = log(Value), color = Pregnant.Sex))+
+  geom_point()+
+  facet_grid(~ Measure, scales = "free")+
+  scale_color_manual(values = cal_palette("dudleya"))
 
-#Number of pregnant individuals in sample
-table(df1[df1$Sex=="Female",]$Pregnant)
+#Make the df a list, split by measure and sex (given sex-specific distributions)
+PAlist <- split(PAdf3, list(PAdf3$Measure, PAdf3$Sex))
 
-ggplot(df1, aes(x = Estradiol, color = Sex))+
-  geom_histogram(bins=45)+
-  facet_grid(~ Sex,scales = "free")
+#A function for fully Bayesian models using brms, with cmdstanr on the backend
+get.brms <- function(x){
+  
+  if(unique(x$Measure) == "Testosterone"){
+    knot.values = c(6,12,18,20,40,70)
+  } else {knot.values = c(6,12,18,20,30,40,50,70)
+    }
+  model.prior <- get_prior(log(Value) ~ 
+                             s(Age, k = length(knot.values)), 
+                           knots = list(Age = knot.values),
+                           data = x, family = gaussian())
+  model.prior$prior[1]<-"normal(0,10"
+                           
+  model <- brms::brm(formula = log(Value) ~ 
+                       s(Age, k = length(knot.values)), 
+               knots = list(Age = knot.values),
+               data = x, family = gaussian(),
+               warmup=1000, iter=2500,prior=model.prior,
+               control = list(max_treedepth = 12),
+               chains=2, cores=2, seed = 1234)
+  model$Measure = unique(x$Measure)
+  model$Sex = unique(x$Sex)
+  model
+}
 
-#Impact of pregnant individuals on sample
-pregplot <- ggplot(df1, aes(x = Age, y = log(Estradiol), 
-                                color = Pregnant))+
-  geom_point(position = "jitter")
+#Apply brms function to list
+brms.models <- map(PAlist,get.brms)
 
-#Weird low values for male Testosterone
-ggplot(dataset, aes(x = log(Testosterone), color = Sex))+
-  geom_histogram()#+
-  #facet_grid(Sex ~.,scales = "free")
+#Create function to gather tidy predicted draws from each model
+getbrmspreds <- function(x){
+  #Age values from original datasets
+  new_data <- data.frame(Age = sort(unique(PAdf3$Age)))
+  
+  preds <- as_tibble(x %>% fitted(newdata = new_data,
+                                  summary = T)) %>% 
+    rename(lnValue = Estimate) %>% 
+    mutate(Age = sort(unique(PAdf3$Age)), Sex = x$Sex, 
+           Measure = x$Measure, Value = exp(lnValue))
+  preds
+}
 
-#Weird low values for both male and female estradiol
+#Apply function to list of brms models
+brms.predvals <- plyr::ldply(map(brms.models, getbrmspreds),
+                             data.frame, .id = NULL) %>% 
+  mutate(Model.Type = "BRMS")
 
-ggplot(df1, aes(x = BMI, y = log(SHBG), color = Sex))+
-  geom_point(position = "jitter")
+#Get gam models using mgcv 
+get.gams <- function(x){
+  model <- mgcv::gam(log(Value) ~ s(Age), 
+                     data = x, method = "REML")
+  model$Measure = unique(x$Measure)
+  model$Sex = unique(x$Sex)
+  model
+}
 
-ggplot(dataset, aes(x = Age, y = log(Testosterone), color = Sex))+
-  geom_point(position = "jitter")
+gam.models <- map(PAlist, get.gams)
 
-#Fully Bayesian model using brms
-est.modbrms <- brm(formula = log(Estradiol) ~ s(Age, by = Sex),
-               data =dataset, family = gaussian(),
-               warmup=1000, iter=2500,
-               chains=2,cores=2)
-summary(est.modbrms)
+getgampreds <- function(x){
+  preds <- predict_gam(x) %>% 
+    rename(lnValue = fit) %>% 
+    mutate(Measure = x$Measure,
+           Sex= x$Sex)
+  preds
+}
 
-#glm model
-est.modglm <- mgcv::gam(log(Estradiol) ~ s(Age, by = Sex), 
-                  data = dataset, method = "REML")
-summary(est.modglm)
+gam.predvalues <- plyr::ldply(map(gam.models, getgampreds),
+                              data.frame, .id = NULL) %>% 
+  mutate(Model.Type = "GAM")
 
-est.modglm2 <- mgcv::gam(log(Estradiol) ~ s(Age, by = Sex)+BMI*Sex, 
-                        data = dataset, method = "REML")
-summary(est.modglm)
+str(gam.predvalues)
+str(brms.predvals)
 
-test <- lm(log(Estradiol) ~ log(Testosterone)*Sex,
-   data = df1)
-summary(test)
+colorset1 = c('Male'='#A2A098','Female'='#5E6B7B')
+colorset2 = c('Male'='#A2A098','NP female'='#5E6B7B','P female'='#233D3F')
+  
+ggplot(brms.predvals, aes(x = Age, y = lnValue, color =Sex))+
+  geom_point(data = PAdf3, aes(x = Age, y = log(Value),
+                               color = Sex))+
+  geom_line(size = 0.7)+
+  geom_ribbon(aes(ymin = Q2.5, ymax =Q97.5, group = Sex),
+              alpha=0.6, linetype = 0) +
+  facet_grid(~Measure,scales = "free")+
+  scale_color_manual(values = colorset1)+
+  labs(title = "predicted values using brms")
 
-shbg.modglm2 <- mgcv::gam(log(SHBG) ~ s(Age, by = Sex),
-                         data = dataset, method = "REML")
-summary(shbg.modglm)
-
-install.packages("tidymv")
-library(tidymv)
-plot_smooths(est.modglm, Age, Sex)
-plot_smooths(shbg.modglm2, Age, Sex)
-#Plot raw data and model output onto single graph, to show I got the goods
-
-
-
-
-
+ggplot(gam.predvalues, aes(x = Age, y = lnValue, 
+                           color = Sex)) + 
+  geom_line(size = 0.7)+
+  geom_point(data = PAdf3, aes(x = Age, y = log(Value),
+                               color = Sex), position = "jitter")+
+  geom_ribbon(aes(ymin = lnValue - se.fit, 
+                  ymax = lnValue + se.fit, 
+                  group = Sex),
+              alpha=0.6, linetype = 0,,show.legend = FALSE) +
+  facet_grid(~ Measure,scales = "free")+
+  scale_color_manual(values = cal_palette("dudleya"))+
+  scale_fill_manual(values = cal_palette("dudleya"))+
+  labs(title = "predicted values using gam")+
+  theme(legend.position = "bottom")
 
