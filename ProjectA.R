36bc4765a857dc4d6187b3c06644bfb9c1d31406
carmenhove <hove.cm@gmail.com>
2022-04-11 16:43:49 -0700

    many changes

--- ProjectA.R
+++ ProjectA.R
@@ -1,4 +1,5 @@
-
+install.packages("visreg")
+library(visreg)
 #PROJECT A
 
 #Identify packages 
@@ -18,6 +19,9 @@ inst = lapply(pkgs, library, character.only = TRUE)
 devtools::install_github("karthik/wesanderson")
 library(wesanderson)
 
+colorset1 = c('Male'='#A2A098','Female'='#5E6B7B')
+colorset2 = c('Male'='#A2A098','NP female'='#5E6B7B','P female'='#233D3F')
+
 #Rstan has been acting squirrely on my OS when I tweak adapt_delta and max treedepth. To get around this, I use cmdstranr on the back-end. 
 install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", 
                                        getOption("repos")))
@@ -31,7 +35,8 @@ options(brms.backend = "cmdstanr")
 
 #Load data from NHANES
 ##Identify BMI, demographic, reproductive health, and hormone data files
-table.names <- sort(c(str_c(c('DEMO','RHQ','BMX'),"_",rep(c('H','I'),each=3)), 
+table.names <- sort(c(str_c(c('DEMO','RHQ','BMX'),"_",
+                            rep(c('H','I'),each=3)), 
                       'TST_H','TST_I'))
 
 #apply nhanes() to extract SAS data frames, transition to R list
@@ -68,9 +73,13 @@ PAdf1 <- full_join(BMI, DEMO) %>%
            (Hysterectomy == 2 | (Sex == "Female" & Age < 20) | Sex == "Male")) %>%  
            #Remove imputed values due to below-threshold detection limit reported
            #Est.cc == 0 & Test.cc == 0) %>% 
-  select(Testosterone,Test.cc,Estradiol,Est.cc,Pregnant,Pregnant.Sex,Age,Sex,BMI) %>% 
+  select(Testosterone,Test.cc,Estradiol,Est.cc,Pregnant,
+         Pregnant.Sex,Age,Sex,BMI,) %>% 
   mutate(Sex = ordered(Sex, levels = c("Male","Female")),
-         Pregnant.Sex = ordered(Pregnant.Sex, levels = c("Male","NP Female","P Female")))
+         Pregnant.Sex = ordered(Pregnant.Sex, levels = c("Male","NP female","P female")),
+         AgeGroup = case_when(Age < 20 ~ "Under 20",
+                              Age >= 20 & Age <=45 ~ "20-45",
+                              Age > 45 ~ "45+"))
 
 #Pivot longer to create "Measure" variable
 PAdf2 <- PAdf1 %>% 
@@ -83,24 +92,112 @@ PAdf2 <- PAdf1 %>%
 PAdf3 <- PAdf2 %>%  
   filter(Est.cc == 0 & Test.cc == 0)
 
-#Make the df a list, split by measure and sex (given sex-specific distributions)
-PAlist <- split(PAdf3, list(PAdf3$Measure, PAdf3$Sex))
+#Remove pregnant individuals
+PAdf4 <- PAdf3 %>% filter(Pregnant == "No")
 
+ggplot(PAdf4 %>% filter(Measure == "Testosterone" & Sex == "Male"), 
+               aes(x = log(Value), color = Sex, fill = Sex))+
+       geom_density(alpha = 0.5)+ 
+       facet_grid(Sex ~ AgeGroup, scales = "free")+
+       scale_color_manual(values = colorset1)+
+       scale_fill_manual(values = colorset1) 
+
+ggplot(PAdf4, aes(x = Age, y = log(Value), color = Sex))+
+  geom_point(position = "jitter")+
+  facet_grid(Measure ~., scales = "free")+
+  scale_color_manual(values = colorset1)+
+  scale_fill_manual(values = colorset1) 
+
+#Make the df a list, split by measure and sex (given sex-specific distributions)
+PAlist <- split(PAdf4, list(PAdf4$Measure,PAdf4$Sex))
+
+##COMPARE LINEAR AND NON-PARAMETRIC MODELS IN GAM (to show that non-linear model is best)
+#Get glm models
+get.glms <- function(x){
+  model <- glm(log(Value) ~ Age+BMI,data = x)
+  model$Measure = unique(x$Measure)
+  model$Sex = unique(x$Sex)
+  model
+}
+
+glm.models <- map(PAlist, get.glms)
+
+#Get gam models using mgcv 
+get.gams <- function(x){
+  if(unique(x$Measure) == "Testosterone"){
+    knot.values = c(6,12,18,20,40,70)
+  } else {knot.values = c(6,12,18,20,30,40,50,70)
+  }
+  model <- mgcv::gam(log(Value) ~ s(Age)+BMI,
+                     k =length(knot.values),
+                     knots = list(x = knot.values),
+                     data = x, method = "REML")
+  model$Measure = unique(x$Measure)
+  model$Sex = unique(x$Sex)
+  model
+}
+
+gam.models <- map(PAlist, get.gams)
+gam.summaries <- map(gam.models, summary)
+
+#Example model comparison showing that smooth model is more appropriate
+anova(glm.models[["Estradiol.Male"]],
+      gam.models[["Estradiol.Male"]],test="Chisq")
+
+#Plot showing major heteroscedasticity in glm models
+plot(glm.models[["Estradiol.Male"]]$fitted.values, 
+     glm.models[["Estradiol.Male"]]$residuals)
+
+#Get gam model predicted values
+getgampreds <- function(x){
+  preds <- predict_gam(x,values = list(Age = unique(PAdf3$Age),
+                                       BMI = median(PAdf3$BMI,
+                                                    na.rm = T))) %>% 
+    rename(lnValue = fit) %>% 
+    mutate(Measure = x$Measure,
+           Sex= x$Sex)
+  preds
+}
+
+#Put predicted values in a dataframe
+gam.predvals <- plyr::ldply(map(gam.models, getgampreds),
+                            data.frame, .id = NULL) %>% 
+  mutate(Model.Type = "GAM")
+
+#Check the structure
+str(gam.predvals)
+
+#Plot gam predicted values
+ggplot(gam.predvals, aes(x = Age, y = lnValue, 
+                           color = Sex, fill = Sex)) + 
+  geom_line(size = 0.7)+
+  #geom_point(data = PAdf3, aes(x = Age, y = log(Value),
+  #                             color = Sex), 
+  #           position = "jitter", alpha = 0.3)+
+  geom_ribbon(aes(ymin = lnValue - se.fit, 
+                  ymax = lnValue + se.fit, 
+                  group = Sex),
+              alpha=0.6, linetype = 0) +
+  facet_grid(~ Measure,scales = "free")+
+  scale_color_manual(values = colorset1)+
+  scale_fill_manual(values = colorset1)
+
+##FULLY BAYESIAN NON-PARAMETRIC REGRESSION MODELS
 #A function for fully Bayesian models using brms, with cmdstanr on the backend
+
 get.brms <- function(x){
-  
   if(unique(x$Measure) == "Testosterone"){
     knot.values = c(6,12,18,20,40,70)
   } else {knot.values = c(6,12,18,20,30,40,50,70)
     }
   model.prior <- get_prior(log(Value) ~ 
-                             s(Age, k = length(knot.values)), 
+                             s(Age, k = length(knot.values)) + BMI, 
                            knots = list(Age = knot.values),
                            data = x, family = gaussian())
   model.prior$prior[1]<-"normal(0,10"
                            
   model <- brms::brm(formula = log(Value) ~ 
-                       s(Age, k = length(knot.values)), 
+                       s(Age, k = length(knot.values)) + BMI, 
                knots = list(Age = knot.values),
                data = x, family = gaussian(),
                warmup=1000, iter=2500,prior=model.prior,
@@ -113,12 +210,16 @@ get.brms <- function(x){
 
 #Apply brms function to list
 brms.models <- map(PAlist,get.brms)
+brms.summaries <- map(brms.models, summary)
+
+check
 
 #Create function to gather tidy predicted draws from each model
 getbrmspreds <- function(x){
   #Age values from original datasets
-  new_data <- data.frame(Age = sort(unique(PAdf3$Age)))
-  
+  new_data <- data.frame(Age = sort(unique(PAdf3$Age)),
+                         BMI = median(PAdf3$BMI,
+                                      na.rm = T))
   preds <- as_tibble(x %>% fitted(newdata = new_data,
                                   summary = T)) %>% 
     rename(lnValue = Estimate) %>% 
@@ -132,35 +233,6 @@ brms.predvals <- plyr::ldply(map(brms.models, getbrmspreds),
                              data.frame, .id = NULL) %>% 
   mutate(Model.Type = "BRMS")
 
-#Get gam models using mgcv 
-get.gams <- function(x){
-  #if(unique(x$Measure) == "Testosterone"){
-  #  knot.values = c(6,12,18,20,40,70)
-  #} else {knot.values = c(6,12,18,20,30,40,50,70)
-  #}
-
-  model <- mgcv::gam(log(Value) ~ s(Age),#, k =length(knot.values)),
-                     #knots = list(x = knot.values),
-                     data = x, method = "REML")
-  model$Measure = unique(x$Measure)
-  model$Sex = unique(x$Sex)
-  model
-}
-
-gam.models <- map(PAlist, get.gams)
-
-getgampreds <- function(x){
-  preds <- predict_gam(x) %>% 
-    rename(lnValue = fit) %>% 
-    mutate(Measure = x$Measure,
-           Sex= x$Sex)
-  preds
-}
-
-gam.predvalues <- plyr::ldply(map(gam.models, getgampreds),
-                              data.frame, .id = NULL) %>% 
-  mutate(Model.Type = "GAM")
-
-colorset1 = c('Male'='#A2A098','Female'='#5E6B7B')
-colorset2 = c('Male'='#A2A098','NP female'='#5E6B7B','P female'='#233D3F')
-  
+#Compare point-estimates
+gam.predvals %>% filter(Age == 29)
+brms.predvals %>% filter(Age ==29)
--- ProjectB.R
+++ ProjectB.R
@@ -1,15 +1,5 @@
 
 #PROJECT B
-install.packages("smotefamily")
-install.packages("car")
-library(smotefamily)
-library(corrplot)
-library(car)
-install.packages("e1071")
-library(e1071)
-install.packages("InformationValue")
-library(InformationValue)
-library(class)
 
 #Upload data as a csv file
 PBdf1 <- read.csv("FullData.csv", stringsAsFactors = FALSE)
@@ -40,12 +30,10 @@ PBdf2 <- PBdf1 %>%
   #Make sure relevant variables are in numeric format
   mutate(across(c(Max.Temp:Precipitation,Day,Month,Departure,Year), as.numeric)) %>%
   #Create binary presence/absence variables for freezing and precipitation
-  mutate(Freezing = case_when(Min.Temp > 33 ~ 0,
-                              Min.Temp <= 33 ~ 1),
-         PrecipB = case_when(Precipitation > 0 ~ 1,
+  mutate(PrecipB = case_when(Precipitation > 0 ~ 1,
                              Precipitation ==0 ~ 0)) %>% 
   #Select out CDD, which only had a value of zero
-  select(PrecipB,Freezing,ends_with("Temp"),HDD,Departure, Day,Year)
+  select(PrecipB, ends_with("Temp"),HDD,Departure, Day,Year)
 
 #Splitting the data
 #Partition data into training and test datasets using a 75%:25% split ratio
@@ -69,49 +57,32 @@ round(prop.table(table(select(training.adj, PrecipB),exclude = NULL)),4)*100
 #Visualizing multicollinearity
 PBdf2 %>% keep(is.numeric) %>% cor() %>% corrplot(type = "upper")
 
-
 #LOGISTICAL REGRESSION
 #Training model #1
-train.mod <- glm(data = training.adj, family = binomial, 
-                 formula = PrecipB ~ .)
-summary(train.mod)
-exp(coef(train.mod))#["Max.Temp"]) #Exponentiated coefficient 
+logitm1 <- glm(data = training.adj, family = binomial, 
+                 formula = PrecipB ~ Departure + Day + Year)
+summary(logitm1)
+vif(logitm1)
+exp(coef(logitm1)[c("Departure","Year","Day")])
 
-#Training model #2
-training.adj2 <- training.adj %>% select(-Ave.Temp)
-train.mod2 <- glm(data = training.adj2, family = binomial, 
-                  formula = PrecipB ~ .)
-
-#Error indicates perfect multicolinearity - which we expected! 
-preds.1 <- predict(train.mod2, testing, type = 'response')
-#Clearly indicates Max.Temp, Min.Temp,and HDD
-summary(train.mod2)
-vif(train.mod2)
-
-training.adj3 <- training.adj2 %>% select(-Max.Temp,-Min.Temp)
-train.mod3 <- glm(data = training.adj3, family = binomial, 
-                  formula = PrecipB ~ Day + Year + HDD)
-summary(train.mod3)
-vif(train.mod3)
-
-preds.2 <- predict(train.mod3, testing, type = 'response')
+logitpred1 <- predict(logitm1, testing, type = 'response')
 
 ideal.cutoff <- optimalCutoff(
   actuals = testing$PrecipB,
-  predictedScores = preds.2,
+  predictedScores = logitpred1,
   optimiseFor = "Both"
 )
 
-preds.3 <- ifelse(preds.2 >= ideal.cutoff,1,0)
+logitpred2 <- ifelse(logitpred1 >= ideal.cutoff,1,0)
 
-preds.table <- table(testing$PrecipB, preds.3)
-sum(diag(preds.table))/nrow(testing)
-#predicting accuracy is 74.46%
+logit.ptable <- table(testing$PrecipB, logitpred2)
 
+sum(diag(logit.ptable))/nrow(testing)
+#predicting accuracy is 77%
 
 #Naive Bayes
-bayes.mod1 <- naiveBayes(PrecipB ~ Day + Year + HDD,
-                         data = training.adj3, laplace = 1)
+bayes.mod1 <- naiveBayes(PrecipB ~ Departure + Day + Year,
+                         data = training.adj, laplace = 1)
 bayes.mod1
 
 bayes.pred1 <- predict(bayes.mod1, testing, type = "class")
@@ -122,25 +93,47 @@ sum(diag(bayes.pred1.table))/nrow(testing)
 
 
 #k-Nearest Neighbors
-testing.knn <- testing %>% select(-Max.Temp,-Min.Temp,-Ave.Temp)
+normalize <- function(x){
+  return((x-mean(x))/sd(x))
+}
 
-train_labels <- as.factor(pull(training.adj3, PrecipB))
-test_labels <- as.factor(pull(testing.knn, PrecipB))
+#Partition data into training and test datasets using a 75%:25% split ratio
+set.seed(1234)
+PBdf.knn <- PBdf2 %>% select(PrecipB, Departure, Day, Year) %>% 
+  mutate(across(c(Departure, Day, Year), normalize)
 
-k.spec <- sqrt(nrow(testing.knn))
+knn_sample_set <- sample(nrow(PBdf.knn),round(nrow(PBdf.knn)*0.75), replace = F)
+knn_training <- PBdf.knn[knn_sample_set,]
+knn_testing <- PBdf.knn[-knn_sample_set,]
 
-knn.preds1 <- knn(train = training.adj3,
-                  test = testing.knn,
-                  cl = train_labels,
-                  k=k.spec
-)
+knn_smote <- SMOTE(knn_training, knn_training$PrecipB)
+knn_training.adj <- knn_smote$data %>% select(-class)
 
-head(knn.preds1)
+train_labels <- as.factor(pull(knn_training, PrecipB))
+test_labels <- as.factor(pull(knn_testing, PrecipB))
 
-knn.predtbl <- table(test_labels, knn.preds1)
-knn.predtbl
+kvalues <- list(1,5,10,15,20,25,30,35,40)
 
-sum(diag(knn.predtbl))/nrow(testing.knn)
-#75.32% predictive accuracy
+get.knearest <- function(x){
+  knn.preds <- knn(train = knn_training %>% 
+                     select(PrecipB, Departure, Day, Year),
+                   test = knn_testing,
+                   cl = train_labels,
+                   k=x
+  )
+  knn.predtbl <- table(test_labels, knn.preds)
+  pred.acc <- sum(diag(knn.predtbl))/nrow(knn_testing)
+  pred.acc
+}
 
-#Can change k to test predictive accuracy 
+knn.df <- plyr::ldply(map(kvalues, get.knearest),data.frame, .id = NULL) %>% 
+  rename(Value = X..i..) %>% 
+  mutate(Kvalue = as.numeric(kvalues),
+         Value = as.numeric(Value))
+
+ggplot(knn.df, aes(x = Kvalue, y = Value))+
+  geom_point()+
+  geom_line()+
+  ylab("Predictive accuracy")+
+  xlab("K-value")
+
